99ed1309c43509990bd8edab50529cbed0b91769
Jonas Blum <jonas.blum@motive.de>
2018-02-23 16:04:58 +0100

    Added .

--- app/code/community/Motive/Easymarketing/Block/Adminhtml/System/Config/Form/Field/Disable.php
+++ app/code/community/Motive/Easymarketing/Block/Adminhtml/System/Config/Form/Field/Disable.php
@@ -1,5 +1,13 @@
 <?php
 
+#######
+# Motive X
+# Sylter Str. 15, 90425 Nürnberg, Germany
+# Telefon: +49 (0)911/49 522 566
+# Mail: info@motive.de
+# Internet: www.motive-x.com
+#######
+
 class Motive_Easymarketing_Block_Adminhtml_System_Config_Form_Field_Disable extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
